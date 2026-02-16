f0bb84c446c4b4f6bc67cf24762165bb872f859c
thomas.spinner0 <thomas.spinner0@gmail.com>
2026-02-16 11:30:57 +0100

    fin exo du cours

---
+++ cours_ece_2026/cours_04/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:formation_flutter/l10n/app_localizations.dart';
+import 'package:formation_flutter/res/app_colors.dart';
+import 'package:formation_flutter/res/app_theme_extension.dart';
+
+import 'package:formation_flutter/screens/homepage/homepage_screen.dart';
+import 'package:formation_flutter/screens/product/product_page.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+   MyApp({super.key});
+
+  final GoRouter _router = GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return const HomePage();
+        },
+      ),
+      GoRoute(
+        path: '/product/:barcode',
+        builder: (BuildContext context, GoRouterState state) {
+          final String barcode = state.pathParameters['barcode']!;
+          return ProductPage(barcode: barcode);
+        },
+      ),
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Open Food Facts',
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      theme: ThemeData(
+        extensions: [OffThemeExtension.defaultValues()],
+        fontFamily: 'Avenir',
+        dividerTheme: DividerThemeData(color: AppColors.grey2, space: 1.0),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedItemColor: AppColors.blue,
+          unselectedItemColor: AppColors.grey2,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+        ),
+        navigationBarTheme: const NavigationBarThemeData(
+          indicatorColor: AppColors.blue,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+    );
+  }
+}
